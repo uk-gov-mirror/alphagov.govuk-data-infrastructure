2e217b338845fcbeeb6b6e76649b8e0e8153602a
aaronfowles <aaron.fowles@digital.cabinet-office.gov.uk>
2026-09-23 10:42:43 +0100

    IA-3367: resolve govuk-publishing drift

--- gcp-govuk-publishing/main.tf
+++ gcp-govuk-publishing/main.tf
@@ -28,9 +28,10 @@ module "managed_project" {
   project_name   = "govuk-publishing"
   project_owners = ["group:gcp-govuk-publishing-owners@digital.cabinet-office.gov.uk"]
   project_editors = [
+    "group:gcp-govuk-publishing-editors@digital.cabinet-office.gov.uk",
     "serviceAccount:govuk-publishing@appspot.gserviceaccount.com",
     "serviceAccount:analytics-processing-dev@system.gserviceaccount.com",
     "serviceAccount:942729121218-compute@developer.gserviceaccount.com",
   ]
-  project_viewers = []
+  project_viewers = ["group:gcp-govuk-publishing-viewers@digital.cabinet-office.gov.uk"]
 }
